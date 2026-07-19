f1e0ed0c184c37e43a51cfa6f68297c618357200
MATHAN S <mathansenthil1320+github@gmail.com>
2026-07-19 20:23:43 +0530

    Time: 0 ms (100%), Space: 43.1 MB (16.58%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String interpret(String command) {
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < command.length(); i++) {
+
+            char ch = command.charAt(i);
+
+            if (ch == 'G') {
+                result.append("G");
+            } else if (ch == '(' && command.charAt(i + 1) == ')') {
+                result.append("o");
+                i++;
+            } else {
+                result.append("al");
+                i += 3;
+            }
+        }
+
+        return result.toString();
+    }
+}
