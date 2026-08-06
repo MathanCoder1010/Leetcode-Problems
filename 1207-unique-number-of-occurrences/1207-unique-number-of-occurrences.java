c45e7ede6fda447da2c5428fc7c2ab8667b10c35
MATHAN S <mathansenthil1320+github@gmail.com>
2026-08-06 08:36:52 +0530

    Time: 2 ms (97.91%), Space: 43.9 MB (18.5%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        Map<Integer, Integer> countMap = new HashMap<>();
+        for (int num : arr) {
+            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+        }
+        
+        Set<Integer> occurrences = new HashSet<>(countMap.values());
+        return countMap.size() == occurrences.size();
+    }
+}
