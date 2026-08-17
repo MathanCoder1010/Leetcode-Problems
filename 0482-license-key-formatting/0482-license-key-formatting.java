e8af8a8e662808feb5cf08a0f193971b9530faa4
MATHAN S <mathansenthil1320+github@gmail.com>
2026-08-17 08:29:11 +0530

    Time: 15 ms (31.45%), Space: 46 MB (95.3%) - LeetHub

---
+++ 0482-license-key-formatting/0482-license-key-formatting.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String licenseKeyFormatting(String s, int k) {
+        s = s.replace("-", "").toUpperCase();
+
+        StringBuilder result = new StringBuilder();
+
+        int count = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            result.append(s.charAt(i));
+            count++;
+
+            if (count == k && i != 0) {
+                result.append("-");
+                count = 0;
+            }
+        }
+
+        return result.reverse().toString();
+    }
+}
